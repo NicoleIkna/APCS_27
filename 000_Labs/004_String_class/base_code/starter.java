f64380140fa3716b25a888f44191edc56398509d
NicoleIkna <Nicoleiknadossian@gmail.com>
2026-09-03 21:09:47 +0000

    update

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,13 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String bob = new String("Happy Birthday, to you.");
+		String will = new String("Happy Birthday, dear");
+		String pete = new String(" Nicole.");
+		System.out.println(bob);
+		System.out.println(bob);
+		System.out.println(will + pete);
+		System.out.println(bob);
+
 	}
 }
