78bbf7299eb7c84197a350e3d591f23c14842d75
NicoleIkna <Nicoleiknadossian@gmail.com>
2026-09-09 20:45:42 +0000

    update

--- 000_Labs/005_int/base_code/starter.java
+++ 000_Labs/005_int/base_code/starter.java
@@ -10,7 +10,11 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		int joe;
-		joe = 589;
-		System.out.print(joe); 
+		joe = 5*7/2%3-1;
+		System.out.println(joe); 
+		int man = 10%3-4*7+2;
+		System.out.println(man);
+		int elin = (3-7/2*5)%10;
+		System.out.println(elin);
 	}
 }
--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,10 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		double celsius;
+		double fahrenheit;
+		fahrenheit = 89.0;
+		celsius = (fahrenheit - 32)*5/9;
+		System.out.println("The Celsius value for "+fahrenheit+" degrees fahrenheit is "+celsius+" degrees.");
 	}
 }
