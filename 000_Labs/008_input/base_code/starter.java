8fc8455997086691aa3b2fec9028a5f7041b97ad
NicoleIkna <Nicoleiknadossian@gmail.com>
2026-09-14 21:04:14 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,21 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your first name?");
+		String name = sc.nextLine();
+		System.out.println("How old are you?");
+		int age = sc.nextInt();
+		System.out.println("What month were you born? (1-12)");
+		int month = sc.nextInt();
+		System.out.println("What day were you born? (1-31)");
+		int day = sc.nextInt();
+		System.out.println("What year were you born?");
+		int year = sc.nextInt();
+		System.out.println("If I have a dollar and two quarters, how much money do I have? (0.0-#)");
+		double money = sc.nextDouble();
+		System.out.println("Your name is " + name + " and you were born on " + month + "/" + day + "/" + year + ".");
+		System.out.println("You are " + age + " years old!!!");
+		System.out.println("You have $" + money + " in your wallet.");
 	}
 }
